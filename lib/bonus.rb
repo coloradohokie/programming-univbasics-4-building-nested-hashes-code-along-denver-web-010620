f6ecfaa61d2d958163bddeb8771eb42f17b282b3
Michael Newman <coloradohokie@gmail.com>
2019-12-15 05:12:41 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -24,6 +24,7 @@ def bonus
 
   epic_tragedy[:montague][:hero][:status] = "dead"
   epic_tragedy[:capulet][:heroine][:status] = "dead"
+  puts epic_tragedy[:montague][:hero]
   return epic_tragedy
   
 
